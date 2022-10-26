2376eb039afdf571f40dfa8646d8b804b03fe05e
Thái Anh Đức <kinaobancan231@gmail.com>
2022-10-26 10:10:15 +0700

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import Header from './Header'
+import {useState,useEffect} from 'react'
 
 function App() {
+  const[show,setShow]=useState(false)
+  const  HandleToggle=()=>{
+    setShow(!show)
+  }
   return (
-    <div className="App">
-      <Header/>
+    <div className="App" style={{padding:20}}>
+      <h2>Test Mount and UnMount</h2>
+      <button onClick={()=>HandleToggle()}>Toggle</button>
+      {show && <Header/>}
+      
     </div>
+    
   );
 }
 
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,48 @@
+import {useState,useEffect} from 'react'
+const tabs=['posts','comments','albums']
 function Header(){
+    const[title,setTitle]=useState('')
+
+    const[posts,setPost]=useState([])
+    const[type,setType]=useState('posts')
+
+    useEffect(()=>{
+      fetch(`https://jsonplaceholder.typicode.com/${type}`)
+        .then(res=>res.json())
+        .then(posts=>{
+          setPost(posts)
+        })
+    },[type])
     return(
     <div>
-        <h2>Đây là phần header </h2>
+      <ul>
+      {
+        tabs.map(tab=>(
+          <button
+            style={type===tab?{
+              backgroundColor:'red'
+            }:{}}
+            key={tab}
+            onClick={()=>setType(tab)}
+          >{tab}</button>
+        ))
+      }
+      </ul>
+        <input 
+        value={title}
+        onChange={e=>setTitle(e.target.value)}
+      />
+      <ul>
+        {posts.map(post=>{
+          return(
+            <div key={post.id}>
+              <li>{post.useId}</li>
+              <h3>{ post.title||post.name}</h3>
+            </div>
+        )})}
+      </ul> 
     </div>
     )
 }
+
 export default Header
