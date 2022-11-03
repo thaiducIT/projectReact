d11850caba9e6343fcf0d179f3c7b2e0fbb054d7
Thái Anh Đức <kinaobancan231@gmail.com>
2022-11-03 10:13:06 +0700

    useCallBack

--- src/Header.js
+++ src/Header.js
@@ -1,23 +1,15 @@
-import {useState,memo} from 'react'
+import {useState,useCallback} from 'react'
 import Navbar from './Navbar'
 
 function Header(){
   const [count,setCount]=useState(0)
-  const [count2,setCount2]=useState(0)
-
-  const handleClick=()=>{
-    setCount(count+1)
-  }
-  const handleClick2=()=>{
-    setCount2(count2+1)
-  }
+  const handleIncrease=useCallback(()=>{
+    setCount(count => count+1)
+  },[count])
   return(
   <div>
-    <Navbar count={count}/>
+    <Navbar onIncrease={handleIncrease}/>
     <h1>{count}</h1>
-    <h1>{count2}</h1>
-    <button onClick={handleClick}>Click1</button>
-    <button onClick={handleClick2}>Click2</button>
   </div>
   )
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,11 @@
 import { memo } from 'react'
 
-function Navbar({count}){
+function Navbar({onIncrease}){
         console.log('re-render')
         return(
                 <div>
-                        <h1>Thái Anh Đức {count}</h1>
+                        <h1>Click Under Button</h1>
+                        <button onClick={onIncrease}>CLick me</button>
                 </div>
         )
 }
