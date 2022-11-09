85cd66182c159c69c5711ae2a26163c10b4e45e1
Thái Anh Đức <kinaobancan231@gmail.com>
2022-11-09 21:42:43 +0700

    useReducer2
    make  useReducer  with action set add and delete

--- src/Header.js
+++ src/Header.js
@@ -1,31 +1,100 @@
-import {useReducer} from 'react'
-const initState=0
-const UP_ACTION='up'
-const DOWN_ACTION='down'
-
-const reduce=(state,action)=>{
-  console.log('reduce running')
-  switch(action){
-    case UP_ACTION:
-    {
-      return state+1
-    }
-    case DOWN_ACTION:
-      {
-        return state-1
-      }
-      default:
-        throw new Error('Invalid action')
+import {useReducer,useRef} from 'react'
+// initial value
+const initState={
+  job:'',
+  jobs:[]
+}
+//action
+const SET_JOB='set_job'
+const ADD_JOB='add_job'
+const DELETE_JOB='delete_job'
+//gia tri kem theo
+const setJob=(payload)=>{
+  return{
+    type:SET_JOB,
+    payload
   }
+}
+const addJob=(payload)=>{
+  return{
+    type:ADD_JOB,
+    payload
+  }
+}
+const deleteJob=(payload)=>{
+  return{
+    type:DELETE_JOB,
+    payload
+  }
+}
+// reducer
+const reduce=(state,action)=>{
+  console.log(state)
+  console.log('re-render')
+  let newState
+  switch(action.type){
+    case SET_JOB:
+    {
+      newState={
+        ...state,
+        job:action.payload
+      }
+      break
+    }
+    case ADD_JOB:
+    {
+      newState={
+        ...state,
+        jobs:[...state.jobs,action.payload]
+      }
+      break
+    }
+    case DELETE_JOB:
+    {
+      const newJobs=[...state.jobs]
+      //splice dùng để xóa 1 phần tử trong mãng splice(index[],1) 
+      //lấy đối số index trong mãng, đối số tiếp theo là số lượng xóa phần tử đứng sau nó
+      newJobs.splice(action.payload,1)
+      newState={
+        ...state,
+        jobs:newJobs
+      }
+      break
+    }
+    default:
+      throw new Error('Action invalid')
+  }
+  return newState;
 }
 
 function Header(){
   const[state,dispatch]=useReducer(reduce,initState)
+  const {job,jobs}=state
+  const inputRef=useRef()
+  const handleSubmit=()=>{
+    dispatch(addJob(job))
+    dispatch(setJob(''))
+    inputRef.current.focus()
+  }
   return(
     <div>
-      <h1>{state}</h1>
-      <button onClick={()=>dispatch(DOWN_ACTION)}>Down</button>
-      <button onClick={()=>dispatch(UP_ACTION)}>Up</button>
+      <input ref={inputRef} value={job} onChange={e=>dispatch(setJob(e.target.value))}/>
+      <button onClick={handleSubmit}>Add</button>
+      <ul>
+        {jobs.map((job,index)=>(
+          <li key={index}>{job}
+          <span
+          onClick={()=>{dispatch(deleteJob(index))}}
+          style={{
+            fontSize:30,
+            color:'red',
+            padding:16,
+            cursor:'pointer'
+          }}
+          >&times;</span>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
